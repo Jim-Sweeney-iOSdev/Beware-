01d26c1e107cc673baaab8973e336273128e6e80
Jim Sweeney <jim.sweeney.ios@gmail.com>
2025-11-28 21:51:51 -0500

    second commit

---
+++ Beware/BewareApp.swift
@@ -0,0 +1,17 @@
+//
+//  BewareApp.swift
+//  Beware
+//
+//  Created by James Sweeney on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BewareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BewareTests/BewareTests.swift
@@ -0,0 +1,17 @@
+//
+//  BewareTests.swift
+//  BewareTests
+//
+//  Created by James Sweeney on 11/28/25.
+//
+
+import Testing
+@testable import Beware
+
+struct BewareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
