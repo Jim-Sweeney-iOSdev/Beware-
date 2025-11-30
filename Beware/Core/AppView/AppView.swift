6b468a9e8b45b20f541b136f82c6a4c4ad7a1f8d
Jim Sweeney <jim.sweeney.ios@gmail.com>
2025-11-29 22:45:01 -0500

    added appviews

---
+++ Beware/Core/AppView/AppView.swift
@@ -0,0 +1,40 @@
+//
+//  AppView.swift
+//  Beware
+//
+//  Created by James Sweeney on 11/29/25.
+//
+
+import SwiftUI
+
+struct AppView: View {
+
+    @AppStorage("showTabbarView") var showTabBar: Bool = false
+
+    var body: some View {
+
+        AppViewBuilder(
+            showTabBar: showTabBar,
+            tabbarView: {
+                ZStack {
+                    Color.red.ignoresSafeArea()
+                    Text("Tabbar")
+                }
+            },
+            onboardingView: {
+                ZStack {
+                    Color.blue.ignoresSafeArea()
+                    Text("Onboarding")
+                }
+            }
+        )
+    }
+}
+
+#Preview("AppView - Tabbar") {
+    AppView(showTabBar: true)
+}
+
+#Preview("AppView - Onboarding") {
+    AppView(showTabBar: false)
+}
